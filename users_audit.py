1e464f5f76847f7506c55d300adadf78d1a9cc56
Todd Murchison <toddm92@gmail.com>
2017-07-25 10:58:14 -0700

    update copyright notice

--- users_audit.py
+++ users_audit.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python
 #
-# PROVIDED AS IS WITH NO WARRANTY OR GUARANTEES
-# Copyright (c) 2017 Evident.io, Inc., All Rights Reserved
+# -------------------------------------------------------------------------------------------
+# Copyright (c) 2013, 2014, 2015, 2016, 2017. Evident.io (Evident). All Rights Reserved. 
+# Evident.io shall retain all ownership of all right, title and interest in and to 
+# the Licensed Software, Documentation, Source Code, Object Code, and API's ("Deliverables"), 
+# including (a) all information and technology capable of general application to Evident.io's
+# customers; and (b) any works created by Evident.io prior to its commencement of any
+# Services for Customer.
+#
+# Upon receipt of all fees, expenses and taxes due in respect of the relevant Services, 
+# Evident.io grants the Customer a perpetual, royalty-free, non-transferable, license to 
+# use, copy, configure and translate any Deliverable solely for internal business operations
+# of the Customer as they relate to the Evident.io platform and products, and always
+# subject to Evident.io's underlying intellectual property rights.
+#
+# IN NO EVENT SHALL EVIDENT.IO BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
+# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF 
+# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF EVIDENT.IO HAS BEEN HAS BEEN
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# EVIDENT.IO SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+# THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+# EVIDENT.IO HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
+# OR MODIFICATIONS.
+# -------------------------------------------------------------------------------------------
+#
+# Description:
 #
 # This script dumps a list of ESP users for an organization to a csv file called,
 # "esp_users_report.csv" in the same directory that the script is executed in.
