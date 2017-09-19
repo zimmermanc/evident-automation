eabcaad52b9de5e044432021806f554c4533e648
Todd Murchison <toddm92@gmail.com>
2017-09-19 15:53:19 -0700

    initial commit

---
+++ autoremediate/aws/policies/AWS_EC2_instances_nonpri_regions_policy.py
@@ -0,0 +1,18 @@
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1505846254000",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateImage",
+                "ec2:DescribeImages",
+                "ec2:DescribeInstances",
+                "ec2:TerminateInstances"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
