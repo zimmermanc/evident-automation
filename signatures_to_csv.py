b712c91f468b5030944ca8b55a70adc93898df7b
John Hutchison <john.hutchison1@gmail.com>
2018-02-13 10:07:01 -0800

    Add script to output default signatures to csv

---
+++ signatures_to_csv.py
@@ -0,0 +1,76 @@
+## Copyright (c) 2013, 2014, 2015, 2016, 2017. Evident.io (Evident). All Rights Reserved. 
+## 
+##   Evident.io shall retain all ownership of all right, title and interest in and to 
+##   the Licensed Software, Documentation, Source Code, Object Code, and API's ("Deliverables"), 
+##   including (a) all information and technology capable of general application to Evident.io's
+##   customers; and (b) any works created by Evident.io prior to its commencement of any
+##   Services for Customer.
+## 
+## Upon receipt of all fees, expenses and taxes due in respect of the relevant Services, 
+##   Evident.io grants the Customer a perpetual, royalty-free, non-transferable, license to 
+##   use, copy, configure and translate any Deliverable solely for internal business operations
+##   of the Customer as they relate to the Evident.io platform and products, and always
+##   subject to Evident.io's underlying intellectual property rights.
+## 
+## IN NO EVENT SHALL EVIDENT.IO BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
+##   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF 
+##   THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF EVIDENT.IO HAS BEEN HAS BEEN
+##   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+## 
+## EVIDENT.IO SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+##   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+##   THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+##   EVIDENT.IO HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
+##   OR MODIFICATIONS.
+## 
+## ---
+#
+# This sript will output a CSV that includes all of the
+# default signatures in ESP
+#
+# Requirements: 
+#
+# * Python3 (Tested with version 3.6.1)
+#   `python --version`
+#
+# * Install the ESP Python SDK
+#   https://github.com/EvidentSecurity/esp-sdk-python2 
+# 
+# * Valid ESP credentials / API keys
+#   https://esp.evident.io/settings/api_keys
+#   export ESP_ACCESS_KEY_ID=<your_access_key>
+#   export ESP_SECRET_ACCESS_KEY=<your_secret_access_key>
+#
+import esp_sdk
+import csv
+
+def main():
+	signatures_api = esp_sdk.SignaturesApi()
+	signatures = signatures_api.list()
+	signatures_to_csv(signatures)
+	exit()
+
+def signatures_to_csv(signatures):
+	with open('signatures.csv', 'w', newline='') as file:
+		writer = csv.writer(file)
+		# Write headers
+		writer.writerow([
+				'NAME', 
+				'IDENTIFIER',
+				'RISK LEVEL',
+				'DESCRIPTION',
+				'RESOLUTION',
+			])
+
+		for sig in signatures:
+			writer.writerow([
+				sig.name, 
+				sig.identifier, 
+				sig.risk_level, 
+				sig.description,
+				sig.resolution,
+			])
+	return 
+
+if __name__ == "__main__":
+	main()
