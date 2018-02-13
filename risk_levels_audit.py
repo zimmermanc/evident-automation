babbd5f8d03dfc95b3fa65e26a0ab11e94d64af0
John Hutchison <john.hutchison1@gmail.com>
2018-02-12 22:59:37 -0800

    Add Risk Levels Audit Script

--- risk_levels_audit.py
+++ risk_levels_audit.py
@@ -1,3 +1,47 @@
+## Copyright (c) 2013, 2014, 2015, 2016, 2017. Evident.io (Evident). All Rights Reserved. 
+## 
+##   Evident.io shall retain all ownership of all right, title and interest in and to 
+##   the Licensed Software, Documentation, Source Code, Object Code, and API's ("Deliverables"), 
+##   including (a) all information and technology capable of general application to Evident.io's
+##   customers; and (b) any works created by Evident.io prior to its commencement of any
+##   Services for Customer.
+## 
+## Upon receipt of all fees, expenses and taxes due in respect of the relevant Services, 
+##   Evident.io grants the Customer a perpetual, royalty-free, non-transferable, license to 
+##   use, copy, configure and translate any Deliverable solely for internal business operations
+##   of the Customer as they relate to the Evident.io platform and products, and always
+##   subject to Evident.io's underlying intellectual property rights.
+## 
+## IN NO EVENT SHALL EVIDENT.IO BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
+##   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF 
+##   THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF EVIDENT.IO HAS BEEN HAS BEEN
+##   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+## 
+## EVIDENT.IO SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+##   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+##   THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+##   EVIDENT.IO HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
+##   OR MODIFICATIONS.
+## 
+## ---
+##
+## This sript will output a CSV that includes risk levels for both built-in
+## and custom signatures for all external accounts. 
+## Requirements: 
+##
+# * Python3 (Tested with version 3.6.1)
+#   `python --version`
+#
+# * Install the ESP Python SDK
+#   https://github.com/EvidentSecurity/esp-sdk-python2 
+# 
+# * Valid ESP credentials / API keys
+#   https://esp.evident.io/settings/api_keys
+#   export ESP_ACCESS_KEY_ID=<your_access_key>
+#   export ESP_SECRET_ACCESS_KEY=<your_secret_access_key>
+#
+
+
 import esp_sdk
 import csv
 
@@ -9,7 +53,7 @@ options = {
 	# ----- Using 'all' will retrieve all built-in signatures
 	# including those that use default risk levels as well as
 	# the custom signatures for all accounts
-	'signatures': 'all'
+	'signatures': 'custom_risk_levels_only'
 }
 
 def main(options):
@@ -34,7 +78,7 @@ def main(options):
 	exit()
 
 def signatures_to_csv(signatures):
-	with open('signatures.csv', 'w', newline='') as file:
+	with open('risk_levels_audit.csv', 'w', newline='') as file:
 		writer = csv.writer(file)
 		# Write headers
 		writer.writerow([
