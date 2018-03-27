ece76fc985521b9343f896e1abb895c6be387d57
John Hutchison <john.hutchison1@gmail.com>
2018-03-27 10:02:21 -0700

    Add script to update AWS role/external_id

---
+++ update_aws_credentials.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018. Evident.io (Evident). All Rights Reserved. 
+# 
+#   Evident.io shall retain all ownership of all right, title and interest in and to 
+#   the Licensed Software, Documentation, Source Code, Object Code, and API's ("Deliverables"), 
+#   including (a) all information and technology capable of general application to Evident.io's
+#   customers; and (b) any works created by Evident.io prior to its commencement of any
+#   Services for Customer.
+# 
+# Upon receipt of all fees, expenses and taxes due in respect of the relevant Services, 
+#   Evident.io grants the Customer a perpetual, royalty-free, non-transferable, license to 
+#   use, copy, configure and translate any Deliverable solely for internal business operations
+#   of the Customer as they relate to the Evident.io platform and products, and always
+#   subject to Evident.io's underlying intellectual property rights.
+# 
+# IN NO EVENT SHALL EVIDENT.IO BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
+#   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF 
+#   THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF EVIDENT.IO HAS BEEN HAS BEEN
+#   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# EVIDENT.IO SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+#   THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+#   EVIDENT.IO HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
+#   OR MODIFICATIONS.
+###############################################################################################
+# # Requirements: 
+#
+# * Python3 (Tested with version 3.6.1)
+#   `python --version`
+#
+# * Install the ESP Python SDK
+#   https://github.com/EvidentSecurity/esp-sdk-python2 
+# 
+# * Valid ESP credentials / API keys
+#   https://esp.evident.io/settings/api_keys
+#   export ESP_ACCESS_KEY_ID=<your_access_key>
+#   export ESP_SECRET_ACCESS_KEY=<your_secret_access_key>
+#
+# Description:
+# 	This script will update an existing AWS account's role credentials. 
+#
+# Instructions:
+# 	Use the 'options' fields below to enter the existing ESP account name, 
+#   the new ARN, and the new external_id. 
+#
+
+import esp_sdk
+
+options = {
+	'account_name': '', # Case sensitive
+	'arn': '',
+	'external_id': ''
+}
+
+def main(options):
+	accounts_api = esp_sdk.ExternalAccountsApi()
+	aws_accounts_api = esp_sdk.ExternalAccountsAmazonApi()
+	account = accounts_api.list(filter={'name_eq': options['account_name']})[0]
+	response = aws_accounts_api.update(account.id, arn=options['arn'], external_id=options['external_id'])
+	print(response)
+	exit()
+	return
+
+if __name__ == '__main__':
+	main(options)
