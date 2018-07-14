34c07bf8623704bd9f989b82b402597245d40ac7
kenchen3 <31941199+kenchen3@users.noreply.github.com>
2018-07-13 19:07:48 -0700

    Update AWS_EC2_security_group_lock_down.py

--- autoremediate/aws/lambda/AWS_EC2_security_group_lock_down.py
+++ autoremediate/aws/lambda/AWS_EC2_security_group_lock_down.py
@@ -72,7 +72,7 @@ def lambda_handler(event, context):
 
     # If the signature didn't report a failure, exit..
     #
-    if status != 'fail':
+    if (status != 'fail' and status != 'warn'):
         print('=> Nothing to do.')
         exit()
 
@@ -90,7 +90,7 @@ def lambda_handler(event, context):
     region = re.sub('_','-',regions['attributes']['code'])
 
     try:
-        sg_id = metadata['attributes']['data']['resource_id']
+        sg_id = alert['data']['attributes']['resource']
     except:
         print('=> No security group to evaluate.')
     else:
